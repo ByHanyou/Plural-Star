075b64a3b91070f88d98ab0a03c6e67592ba07fc
TheHanyou <the1hanyou@gmail.com>
2026-05-17 16:05:55 -0700

    @mentions in profiles/CFs/journals with tap-to-open. Journal Templates sub-tab. Member count subtitle. TextInput crash hardening, sheet nav-bar overlap, keyboard footer overlap, header inset. Photo Picker replaces Document Picker for avatars/banners. Notification labels localized. Custom text fields multiline auto-expand. Noteboard write enabled in view mode. Front note clear via quick edit fix. Text-size scales across settings + tab bar. Dyslexia-friendly font toggle (default on; rendering pending).

--- android/app/src/main/java/com/pluralspace/app/MainApplication.kt
+++ android/app/src/main/java/com/pluralspace/app/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
+import com.facebook.react.common.assets.ReactFontManager
 
 class MainApplication : Application(), ReactApplication {
 
@@ -28,6 +29,7 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
+        ReactFontManager.getInstance().addCustomFont(this, "OpenDyslexic", R.font.opendyslexic)
         SoLoader.init(this, OpenSourceMergedSoMapping)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             load()
